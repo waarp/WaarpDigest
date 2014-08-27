d19175f005e62e06bf6f6443fbe028c0633178b3
fredericBregier <fredbregier@free.fr>
2014-08-27 16:59:24 +0200

    Fix version

--- src/main/java/org/waarp/common/digest/Version.java
+++ src/main/java/org/waarp/common/digest/Version.java
@@ -3,7 +3,7 @@ package org.waarp.common.digest;
 /** Provides the version information of Waarp Digest. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "1.1.8";
+ public static final String ID = "2.0.0";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
